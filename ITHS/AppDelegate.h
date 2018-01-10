6db62a7aabbfd94e5e9ecbff7bfa899edaea4be3
SewerSnake <ericgroseclos@hotmail.com>
2018-01-10 10:45:14 +0100

    Initial Commit

---
+++ ITHS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ITHS
+//
+//  Created by Eric Groseclos on 2018-01-10.
+//  Copyright © 2018 Eric Groseclos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ITHS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ITHS
+//
+//  Created by Eric Groseclos on 2018-01-10.
+//  Copyright © 2018 Eric Groseclos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
